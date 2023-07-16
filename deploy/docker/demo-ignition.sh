f77cd17d64e846757886f7b20a42656ca2fc4e86
Valery Lobachev <valery.lobachev@gmail.com>
2023-07-16 13:28:16 +0300

    fix(deploy): fixed demo-ignition version

--- deploy/docker/demo-ignition.sh
+++ deploy/docker/demo-ignition.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export BACKEND_VERSION="0.5.0-SNAPSHOT"
+export BACKEND_VERSION="0.5.0"
 docker run -it --network "annette_default" \
    -e JAVA_OPTS='-Dconfig.file=/opt/docker/conf/application.demo.conf' \
    annetteplatform/demo-ignition:$BACKEND_VERSION
