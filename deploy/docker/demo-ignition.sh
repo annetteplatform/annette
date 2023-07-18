8207e0b69c0d43ba833ecdb5866a046a52d6ab31
Valery Lobachev <valery.lobachev@gmail.com>
2023-07-18 12:01:51 +0300

    feature(deploy): update version in deploy

--- deploy/docker/demo-ignition.sh
+++ deploy/docker/demo-ignition.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export BACKEND_VERSION="0.5.0"
+export BACKEND_VERSION="0.5.1"
 docker run -it --network "annette_default" \
    -e JAVA_OPTS='-Dconfig.file=/opt/docker/conf/application.demo.conf' \
    annetteplatform/demo-ignition:$BACKEND_VERSION
--- deploy/docker/deploy.sh
+++ deploy/docker/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export FRONTEND_VERSION="0.5.0"
-export BACKEND_VERSION="0.5.0"
+export FRONTEND_VERSION="0.5.1"
+export BACKEND_VERSION="0.5.1"
 docker-compose --project-name annette up -d
